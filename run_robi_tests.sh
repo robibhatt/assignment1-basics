0f5aa2a7cbe2ad7d7a2d450d82c234da8186dbf4
Robi Bhattacharjee <robibhattacharjee@mac.speedport.ip>
2025-10-07 21:00:06 +0200

    added tokenizer tests to script

--- run_robi_tests.sh
+++ run_robi_tests.sh
@@ -14,4 +14,5 @@ pwd
 nvidia-smi # only if you requested gpus
 
 uv run pytest robi_tests/
-uv run pytest tests/test_train_bpe.py 
+uv run pytest tests/test_train_bpe.py
+uv run pytest tests/test_tokenizer.py
