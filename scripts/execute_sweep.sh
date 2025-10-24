de4165bedea69ef8f2b0d74da4db7a78736d7fdd
Robi Bhattacharjee <robibhattacharjee@uni-vpn241.vpn.uni-tuebingen.de>
2025-10-24 08:01:59 +0200

    added sweep code

---
+++ scripts/execute_sweep.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=ibor_train_bpe
+#SBATCH --output=logs/train_bpe.out
+#SBATCH --error=logs/train_bpe.err
+#SBATCH --partition=2080-galvani
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=100G
+#SBATCH --time=30:00:00
+
+# Diagnostic and Analysis Phase - please leave these in.
+scontrol show job $SLURM_JOB_ID
+pwd
+nvidia-smi # only if you requested gpus
+
+uv run wandb agent robibhatt/stanford_class_assignment_1/abcd1234
